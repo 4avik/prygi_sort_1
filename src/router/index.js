3ab35bf102c829d4cd544b578ebe04e895219ba2
Liis Alber-Jaansalu <liisalber@MSI>
2023-09-21 23:09:12 +0300

    fixed add product

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/Index.vue'
-import AddProduct from '../views/AddProduct.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
